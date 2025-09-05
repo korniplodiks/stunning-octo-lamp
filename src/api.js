54a975f1353c3a71a057686cd10f0870fa90b2bf
korniplodiks <korniplodiks@users.noreply.github.com>
2025-09-06 02:08:48 +0300

    ci: update API documentation

--- src/api.js
+++ src/api.js
@@ -266,3 +266,30 @@ class API {
 
 // Update 49
 module.exports = API;
+
+
+// API module for LampFramework
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 54
+module.exports = API;
